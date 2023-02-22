bd29d28852fc5e216e5778829ad6e57732d90247
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2023-02-22 21:03:41 +0100

    add window movements and line highlight

--- lua/philippschwarzmueller/remap.lua
+++ lua/philippschwarzmueller/remap.lua
@@ -1,4 +1,14 @@
 local nnoremap = require("philippschwarzmueller.keymap").nnoremap
 
+-- OPEN NETRW
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+-- REPLACE ALL OCCURENCES OF A WORD IN CURR BUFFER
 nnoremap("<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+-- SPLIT WINDOW AND MOVE BETWEEN THEM
+nnoremap("<leader>wv", ":vsp<CR>")
+nnoremap("<leader>wf", ":sp<CR>")
+nnoremap("<leader>wh", ":wincmd h<CR>")
+nnoremap("<leader>wj", ":wincmd j<CR>")
+nnoremap("<leader>wk", ":wincmd k<CR>")
+nnoremap("<leader>wl", ":wincmd l<CR>")
+nnoremap("<leader>wc", ":clo<CR>")
--- lua/philippschwarzmueller/set.lua
+++ lua/philippschwarzmueller/set.lua
@@ -1,6 +1,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "81"
+vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
