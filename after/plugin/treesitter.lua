cf0578bddb56b3772aa94056c816b1d27cedfd9e
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2023-02-22 19:44:18 +0100

    add automatic syntax highlighting

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,6 @@
+require('nvim-treesitter.configs').setup {
+	ensure_installed = { c, lua, vim },
+	highlight = {
+		enable = true,
+	}
+}
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/pschwarz/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/pschwarz/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/pschwarz/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/pschwarz/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/pschwarz/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/macbook/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/macbook/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/macbook/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/macbook/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/macbook/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,77 +76,77 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["42header"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/42header",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/42header",
     url = "https://github.com/42Paris/42header"
   },
   LuaSnip = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["norminette-vim"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/norminette-vim",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/norminette-vim",
     url = "https://github.com/alexandregv/norminette-vim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   syntastic = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/syntastic",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/syntastic",
     url = "https://github.com/vim-syntastic/syntastic"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/Users/pschwarz/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/Users/macbook/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   }
 }
