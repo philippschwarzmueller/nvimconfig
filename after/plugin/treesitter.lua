b1e5031db76cfb51c55daa228e244f25019d527d
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2023-04-28 10:04:42 +0200

    add treesitter for rust js and ts

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-	ensure_installed = { c, lua, vim },
+	ensure_installed = { c, lua, vim, rust, javascript, typescript },
 	highlight = {
 		enable = true,
 	}
