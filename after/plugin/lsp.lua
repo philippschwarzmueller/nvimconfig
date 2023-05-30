1fe05c695ea3a23cf7e5242662596a7b89ceb7f5
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-05-30 18:29:36 +0200

    add lua lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,6 +16,36 @@ require('lspconfig').ccls.setup{
 	end,
 }
 
+require('lspconfig').lua_ls.setup {
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				globals = {'vim'},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+	on_attach = function()
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
+		vim.keymap.set('n', 'F', vim.lsp.buf.format, {buffer = 0})
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0})
+		vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer = 0})
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer = 0})
+		vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, {buffer = 0})
+		vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
+		vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {buffer = 0})
+	end
+}
+
 require('lspconfig').rust_analyzer.setup({
 	capabilities = capabilities,
 	on_attach = function()
