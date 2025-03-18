c785b6a2d6ca72e18bc154f2615d0487d44190f6
Philipp Schwarzmueller <philipp.schwarzmueller@gmail.com>
2025-03-18 19:36:33 +0100

    add go

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,6 +16,20 @@ require('lspconfig').ccls.setup {
   end,
 }
 
+require('lspconfig').gopls.setup({
+  capabilities = capabilities,
+  on_attach = function()
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+    vim.keymap.set('n', 'F', vim.lsp.buf.format, { buffer = 0 })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, { buffer = 0 })
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = 0 })
+  end,
+})
+
 require('lspconfig').tsserver.setup({
   init_options = {
     preferences = {
