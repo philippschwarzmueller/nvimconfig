d4391297310fa0d1236ddadb35fb75ae9ea7e456
pschwarz <pschwarz@pop-os.fritz.box>
2025-03-28 07:52:55 +0100

    add lsp setupt

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      local lspkind = require("lspkind")
+      local kind_formatter = lspkind.cmp_format({
+        mode = "symbol_text",
+        menu = {
+          buffer = "[buf]",
+          nvim_lsp = "[LSP]",
+          nvim_lua = "[api]",
+          path = "[path]",
+        }
+      })
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "lazydev", group_index = 0, },
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({select = true})
+        }),
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          expandable_indicator = true,
+          format = function(entry, vim_item)
+            vim_item = kind_formatter(entry, vim_item)
+          return vim_item
+          end,
+        },
+      })
+    end,
+  }
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,110 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "luvit-meta/library",      words = { "vim%.uv" } },
+            { path = "/usr/share/awesome/lib/", words = { "awesome" } },
+          }
+        }
+      },
+      { "Bilal2453/luvit-meta", lazy = true },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+      "stevearc/conform.nvim",
+    },
+    config = function()
+      local capabilities = nil
+      if pcall(require, "cmp_nvim_lsp") then
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
+      end
+      local lspconfig = require("lspconfig")
+      local servers = {
+        lua_ls = true,
+        ts_ls = {
+          root_dir = require("lspconfig").util.root_pattern("package.json"),
+          single_file = false,
+          server_capabilities = {
+            documentFormattingProvider = false
+          }
+        }
+      }
+      local servers_to_install = vim.tbl_filter(function(key)
+        local t = servers[key]
+        if type(t) == "table" then
+          return not t.manual_install
+        else
+          return t
+        end
+      end, vim.tbl_keys(servers))
+      require("mason").setup()
+      local ensure_installed = {
+        "stylua",
+        "lua_ls"
+      }
+      vim.list_extend(ensure_installed, servers_to_install)
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+      for name, config in pairs(servers) do
+        if config == true then
+          config = {}
+        end
+        config = vim.tbl_deep_extend("force", {}, {
+          capabilities = capabilities,
+        }, config)
+        lspconfig[name].setup(config)
+      end
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "need valid client")
+          local settings = servers[client.name]
+          if type(settings) ~= "table" then
+            settings = {}
+          end
+          local builtin = require("telescope.builtin")
+          vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+          vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+          vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+          vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
+
+          if settings.server_capabilities then
+            for k, v in pairs(settings.server_capabilities) do
+              if v == vim.NIL then
+                --- @diagnostic disable-next-line: cast-local-type
+                v = nil
+              end
+              client.server_capabilities[k] = v
+            end
+          end
+        end
+      })
+      -- add auto format here
+      require("lsp_lines").setup()
+      vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+      vim.keymap.set("", "<leader>l", function()
+        local config = vim.diagnostic.config() or {}
+        if config.virtual_text then
+          vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
+        else
+          vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+        end
+      end, { desc = "Toggle lsp_lines" })
+    end
+  }
+}
+
+--[[ return {
+  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
@@ -93,4 +198,4 @@ return {
       }
     end,
   },
-}
+} ]]--
